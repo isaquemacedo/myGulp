c7e18dc5e5342f1743f93b106a3ce1b2940a152c
Isaque Macedo <isaque.mace@gmail.com>
2016-08-11 16:44:04 -0300

    README com instruções, função para compilar o lass e nova estrutura

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # myGulp
+
+Comandos para instalação, antes de tudo installe o gulp de forma global:
+
+``
+$ npm install -g gulp
+``
+
+No diretório do projeto, execute os comandos:
+
+```sh
+$ npm install
+$ bower install
+```
+
+Lista das principais tarefas:
+
+1. `$ gulp start`: inicia servidor local para desenvolvimento;
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var reload = browserSync.reload;
 
-// Static server
-gulp.task('start', function() {
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
     browserSync.init({
-        server: {
-            baseDir: "./"
-        }
+        server: "./app"
     });
+
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on("change", reload);
 });
 
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
